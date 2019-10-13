d9af22368c80d1f93cea81411fd8af66870c143c
Peter Sieg <chasingmaxwell@gmail.com>
2019-10-13 00:29:10 -0400

    fix: add install_requires to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from setuptools import setup, find_packages
 from identifyPropositions import __version__
+import os
+import io
+
+
+def read(filename):
+    filepath = os.path.join(os.path.dirname(__file__), filename)
+    with io.open(filepath, mode="r", encoding="utf-8") as f:
+        return f.read()
+
 
 setup(
     name="syllogio-identifyPropositions",
@@ -12,6 +21,7 @@ setup(
     packages=find_packages(exclude=["tests"]),
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
+    install_requires=read("requirements.txt").splitlines(),
     zip_safe=False,
     entry_points={
         "console_scripts": [
