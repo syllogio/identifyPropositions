f5e7bd01029c755264631a260e1b27647b7f3033
semantic-release <semantic-release>
2019-10-13 00:53:58 -0400

    0.0.2
    Automatically generated by python-semantic-release

--- identifyPropositions/__init__.py
+++ identifyPropositions/__init__.py
@@ -1,6 +1,6 @@
 from .util import parseClause, clauseTokensToString, getAllDescendantTokens  # noqa
 from .identify import identify  # noqa
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 __all__ = ["parseClase", "clauseTokensToString", "getAllDescendantTokens", "identify"]
