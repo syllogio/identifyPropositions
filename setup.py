7578d4fbda2ba1b77dec65a559fafc31f51e96c7
Peter Sieg <chasingmaxwell@gmail.com>
2019-10-31 17:02:23 -0400

    refactor: simplify install_requires

--- setup.py
+++ setup.py
@@ -1,13 +1,5 @@
 from setuptools import setup, find_packages
 from identifyPropositions import __version__
-import os
-import io
-
-
-def read(filename):
-    filepath = os.path.join(os.path.dirname(__file__), filename)
-    with io.open(filepath, mode="r", encoding="utf-8") as f:
-        return f.read()
 
 
 setup(
@@ -21,7 +13,7 @@ setup(
     packages=find_packages(exclude=["tests"]),
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    install_requires=read("requirements.txt").splitlines(),
+    install_requires=open("requirements.txt").read().splitlines(),
     zip_safe=False,
     entry_points={
         "console_scripts": [
